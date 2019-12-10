6c37ae6eef0f29c0f5b3f8cd8aebe7549875fa3e
pabolmasov <pavel.abolmasov@gmail.com>
2019-12-10 20:25:27 +0200

    added CONSTVTEST to decs

--- decs.h
+++ decs.h
@@ -80,8 +80,9 @@
 #define ENTWAVE_TEST 8
 #define IC_TORUS 9
 #define AMIRSOURCE 10
+#define CONSTVTEST 11
 
-#define WHICHPROBLEM TORUS_PROBLEM
+#define WHICHPROBLEM BONDI_PROBLEM_2D
 #define DOKTOT   0
 
 /** here are the few things that we change frequently **/
@@ -114,7 +115,7 @@
 #define PERIODIC (0)          /* whether or not to use periodic boundary conditions */
 #define OUTFLOW  (0)          /* whether or not to use outflow boundary conditions in all directions */
 #elif WHICHPROBLEM == TORUS_PROBLEM
-#define N1       (256)         /* number of physical zones in X1-direction */  //change back to 256x256
+#define N1       (128)         /* number of physical zones in X1-direction */  //change back to 256x256
 #define N2       (256)         /* number of physical zones in X2-direction */
 #define N3       (1)          /* number of physical zones in X3-direction */
 #define GR       (1)          /* whether or not to use GR */
@@ -149,6 +150,15 @@
 #define INFLOW   (0)          /* whether or not to allow inflow at boundaries */
 #define PERIODIC (0)          /* whether or not to use periodic boundary conditions */
 #define OUTFLOW  (0)          /* whether or not to use outflow boundary conditions in all directions */
+#elif WHICHPROBLEM == BONDI_PROBLEM_1D
+#define N1       (64)         /* number of physical zones in X1-direction */
+#define N2       (1)         /* number of physical zones in X2-direction */
+#define N3       (1)          /* number of physical zones in X3-direction */
+#define GR       (1)          /* whether or not to use GR */
+#define BL       (1)          /* whether or not to use BL coords */
+#define INFLOW   (0)          /* whether or not to allow inflow at boundaries */
+#define PERIODIC (0)          /* whether or not to use periodic boundary conditions */
+#define OUTFLOW  (0)          /* whether or not to use outflow boundary conditions in all directions */
 #elif WHICHPROBLEM == SNDWAVE_TEST
 #define N1       (32)         /* number of physical zones in X1-direction */
 #define N2       (1)          /* number of physical zones in X2-direction */
@@ -159,7 +169,16 @@
 #define PERIODIC (1)          /* whether or not to use periodic boundary conditions */
 #define OUTFLOW  (0)          /* whether or not to use outflow boundary conditions in all directions */
 #elif WHICHPROBLEM == ENTWAVE_TEST
-#define N1/*ent*/(32)         /* number of physical zones in X1-direction */
+#define N1     (128)         /* number of physical zones in X1-direction */
+#define N2       (1)        /* number of physical zones in X2-direction */
+#define N3       (1)          /* number of physical zones in X3-direction */
+#define GR       (0)          /* whether or not to use GR */
+#define BL       (0)          /* whether or not to use BL coords */
+#define INFLOW   (1)          /* whether or not to allow inflow at boundaries */
+#define PERIODIC (1)          /* whether or not to use periodic boundary conditions */
+#define OUTFLOW  (0)          /* whether or not to use outflow boundary conditions in all directions */
+#elif WHICHPROBLEM == CONSTVTEST
+#define N1     (128)         /* number of physical zones in X1-direction */
 #define N2       (1)        /* number of physical zones in X2-direction */
 #define N3       (1)          /* number of physical zones in X3-direction */
 #define GR       (0)          /* whether or not to use GR */
@@ -222,7 +241,7 @@
 #define SMALL	(1.e-20)
 
 /* Max. value of gamma, the lorentz factor */
-#define GAMMAMAX (50.)
+#define GAMMAMAX (100.)
 
 /* maximum fractional increase in timestep per timestep */
 #define SAFE	(1.3)
